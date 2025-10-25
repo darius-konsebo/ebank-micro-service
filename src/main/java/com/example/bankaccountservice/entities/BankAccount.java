e861d01ad60e29b89cd5fe73b978e1e217fe2473
darius-konsebo <dariuskonsebo@gmail.com>
2025-10-25 18:44:59 +0100

    Entité JPA Compte

---
+++ src/main/java/com/example/bankaccountservice/entities/BankAccount.java
@@ -0,0 +1,23 @@
+package com.example.bankaccountservice.entities;
+
+import com.example.bankaccountservice.enums.AccountType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+@Entity
+@lombok.Data
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private String currency;
+    private AccountType type;
+}
