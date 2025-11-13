d5819470bfdc139b7450c55bd870695d69d53a46
darius-konsebo <dariuskonsebo@gmail.com>
2025-11-13 13:58:41 +0100

    packages DTO et Service

---
+++ src/main/java/com/example/bankaccountservice/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.example.bankaccountservice.service;
+
+import com.example.bankaccountservice.dto.BankAccountRequestDTO;
+import com.example.bankaccountservice.dto.BankAccountResponseDTO;
+import com.example.bankaccountservice.entities.BankAccount;
+
+public interface AccountService {
+    BankAccountResponseDTO addAccount (BankAccountRequestDTO bankAccountDTO);
+}
---
+++ src/main/java/com/example/bankaccountservice/service/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.bankaccountservice.service;
+
+import com.example.bankaccountservice.dto.BankAccountRequestDTO;
+import com.example.bankaccountservice.dto.BankAccountResponseDTO;
+import com.example.bankaccountservice.entities.BankAccount;
+import com.example.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class AccountServiceImpl implements AccountService {
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    @Override
+    public BankAccountResponseDTO addAccount(BankAccountRequestDTO bankAccountDTO) {
+        BankAccount bankAccount = BankAccount.builder()
+                .id(UUID.randomUUID().toString())
+                .createdAt(new Date())
+                .balance(bankAccountDTO.getBalance())
+                .type(bankAccountDTO.getType())
+                .currency(bankAccountDTO.getCurrency())
+                .build();
+        BankAccount saveBankAccount = bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO = BankAccountResponseDTO.builder()
+                .id(saveBankAccount.getId())
+                .type(saveBankAccount.getType())
+                .createdAt(saveBankAccount.getCreatedAt())
+                .currency(bankAccount.getCurrency())
+                .balance(saveBankAccount.getBalance())
+                .build();
+        return bankAccountResponseDTO;
+    }
+}
